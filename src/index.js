1f3ecb6c6734721a79e1ae8cc032c9f5f26d8f23
malishX <hschool510@gmail.com>
2021-10-09 15:54:36 +0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom  from "react-dom";
+import ReactDom from "react-dom";
 
 //stateless functional component
 //always return jsx
@@ -8,8 +8,29 @@ import ReactDom  from "react-dom";
 //    return <h4>Lets finish component and complete course today in 6 hours</h4>;
 // }
 
-const Greeting = () => {
-   return React.createElement('h1', {}, 'hellow world');
+// const Greeting = () => {
+//    return React.createElement('h1', {}, 'hellow world');
+// }
+
+function Greeting() {
+   return (
+      <>
+         <div onClick>
+            <h1>
+               hello work finish
+            </h1>
+            <ul>
+               <li>
+                  <a href='#'>Hellow world</a>
+               </li>
+               <img src='' alt='' />
+               <input type='text' name='' id=''/>
+            </ul>
+         </div>
+         <div></div>
+      </>
+   );
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root')); 
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
