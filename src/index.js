b8fa47c335e68aa6a43644de2a88fe62b81c1517
malishX <hschool510@gmail.com>
2021-10-09 14:43:35 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDom  from "react-dom";
 
-function Greeting(){
-   return <h4>THis my first component</h4>;
+//stateless functional component
+//always return jsx
+
+// function Greeting(){
+//    return <h4>Lets finish component and complete course today in 6 hours</h4>;
+// }
+
+const Greeting = () => {
+   return React.createElement('h1', {}, 'hellow world');
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<Greeting/>, document.getElementById('root')); 
