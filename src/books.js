7e8745fb852f58686c5a07660aa8d81dedd3cc1a
malishX <hschool510@gmail.com>
2021-10-11 01:38:55 +0400

    changes

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+   {
+         id: 1,
+         title :'The Storyteller:Tales of Life and Music',
+         author :'Dave Grohl',
+         img : 'https://images-na.ssl-images-amazon.com/images/I/81kosCB1luL._AC_UL200_SR200,200_.jpg' ,
+   },
+   {
+         id : 2,
+         title :'And Away',
+         author :'Richard Osman',
+         img : 'https://images-eu.ssl-images-amazon.com/images/I/71yBZOvXBVL._AC_UL200_SR200,200_.jpg' ,
+   },{
+         id: 3,
+         title :'Silverview: John Le CarréSilverview: John Le Carré',
+         author :'John le Carré',
+         img : 'https://images-eu.ssl-images-amazon.com/images/I/91NcJLzUc2L._AC_UL200_SR200,200_.jpg' ,
+   },
+
+];
--- src/index.js
+++ src/index.js
@@ -1,38 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-
+import { books } from "./books";
+import Book from './Book'
+import { greeting } from "./testing/testing";
 
 //Nested Components
 //css
 import './index.css'
 
-//creating object array
-
-const books = [
-   {
-         id: 1,
-         title :'The Storyteller:Tales of Life and Music',
-         author :'Dave Grohl',
-         img : 'https://images-na.ssl-images-amazon.com/images/I/81kosCB1luL._AC_UL200_SR200,200_.jpg' ,
-   },
-   {
-         id : 2,
-         title :'And Away',
-         author :'Richard Osman',
-         img : 'https://images-eu.ssl-images-amazon.com/images/I/71yBZOvXBVL._AC_UL200_SR200,200_.jpg' ,
-   },{
-         id: 3,
-         title :'Silverview: John Le CarréSilverview: John Le Carré',
-         author :'John le Carré',
-         img : 'https://images-eu.ssl-images-amazon.com/images/I/91NcJLzUc2L._AC_UL200_SR200,200_.jpg' ,
-   },
-
-];
-
-
-
 function BookList() {
+   console.log(greeting)
    return (
       <section className='booklist'>
          {
@@ -50,30 +27,5 @@ function BookList() {
    );
 }
 
-const Book = (props) => {
-   const {img, title, author} = props
-   const clickHandler = (e) => {
-      console.log(e.target)
-      alert('Hello world')
 
-   }
-
-   const complexExampele = (author) => {
-
-   }
-   return (<article className='book' onMouseOver={()=>{
-      console.log(title)
-
-   }}>
-      <img 
-      src= {img}
-      alt=''
-      />
-      <h1 onClick={()=> console.log(title)}>{title}</h1>      
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>Examnple</button>
-      <button type="button" onClick={() =>complexExampele(author)}>more complex</button>
-   </article>
-   );
-};
 ReactDom.render(<BookList />, document.getElementById('root'));
